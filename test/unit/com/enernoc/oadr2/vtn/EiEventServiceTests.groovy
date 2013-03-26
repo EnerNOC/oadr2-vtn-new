b87db14f2b0bfa1d1aa6febb53bf5a437599d756
Thom Nichols <tmnichols@gmail.com>
2013-03-26 11:31:53 -0400

    moved package for unit tests

---
+++ test/unit/com/enernoc/oadr2/vtn/EiEventServiceTests.groovy
@@ -0,0 +1,19 @@
+package com.enernoc.oadr2.vtn
+
+
+
+import grails.test.mixin.*
+
+import org.enernoc.oadr2.vtn.EiEventService;
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(EiEventService)
+class EiEventServiceTests {
+
+    void testSomething() {
+        fail "Implement me"
+    }
+}
---
+++ test/unit/com/enernoc/oadr2/vtn/PushServiceTests.groovy
@@ -0,0 +1,19 @@
+package com.enernoc.oadr2.vtn
+
+
+
+import grails.test.mixin.*
+
+import org.enernoc.oadr2.vtn.PushService;
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(PushService)
+class PushServiceTests {
+
+    void testSomething() {
+        fail "Implement me"
+    }
+}
