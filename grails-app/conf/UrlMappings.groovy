b476be212424e0019c928af585ccb86150fa2b5d
Thom Nichols <tmnichols@gmail.com>
2013-04-06 15:07:25 -0400

    OADR Test console for submitting service operations

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -10,6 +10,8 @@ class UrlMappings {
 		"/OpenADR2/Simple/EiEvent"(controller : "eiEvent") {
             action = [POST:"post"]
         }
+        
+        "/OADRTest/template/$service/$template"(controller: "OADRTest", action: "template")
 
 		"/"( controller : "home" )
 		
