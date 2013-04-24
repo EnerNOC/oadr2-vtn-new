6aee83015f0d72fba9eccc0a96487f1bab1b3c7a
Thom Nichols <tmnichols@gmail.com>
2013-04-24 11:10:01 -0400

    removed annoying SLF4J warning

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -10,8 +10,7 @@ grails.project.dependency.resolution = {
 //	pom true
     // inherit Grails' default dependencies
     inherits("global") {
-        // specify dependency exclusions here; for example, uncomment this to disable ehcache:
-        // excludes 'ehcache'
+        excludes "slf4j-log4j12"
     }
     log "error" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
     checksums true // Whether to verify checksums on resolve
