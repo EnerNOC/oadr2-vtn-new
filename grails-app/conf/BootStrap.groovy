25e9d93072a198d4ed8b2a7c9a18d388f0dfef03
Thom Nichols <tmnichols@gmail.com>
2013-07-05 13:41:46 -0400

    cleaned up bootstrap

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -10,41 +10,46 @@ import com.enernoc.open.oadr2.vtn.Ven
  * @author Yang Xiang
  */
 class BootStrap {
+    
+    def grailsApplication
 
     def init = { servletContext ->
 //        log.info  "ENV: ------------- ${Environment.current.name}"
         if ( Environment.current.name != 'development' ) {
-            log.info "Not in dev, Skipping Bootstrap!"
+            log.info "+++++ Not in dev, Skipping Bootstrap!"
             return
         }
         try {
             if ( Ven.count() ) {
-                log.info "Bootstrap data appears to already be created!"
+                log.info "+++++ Bootstrap data appears to already be created!"
                 return
             }
             
-    		def pro1 = new Program(name:"test-program-one", marketContext:"http://test-uri-one.com")
-    		def pro2 = new Program(name:"test-program-two", marketContext:"http://test-uri-two.com")
-    		def pro3 = new Program(name:"test-program-three", marketContext:"http://test-uri-three.com")
-    		//pro1.save()
-    		//pro2.save()
-    	
-    		def Ven1 = new Ven(venID:"test-customer-one", name:"test-name-one", clientURI:"http://test-client-uri-one.com")
-    		pro1.addToVens(Ven1)
-    		pro1.save();
-    		
-    		def Ven2 = new Ven(venID:"test-customer-two", name:"test-name-two", clientURI:"http://test-client-uri-two.com")
-    		pro2.addToVens(Ven2)
-    		pro2.save();
-    		
-    		def Ven3 = new Ven(venID:"test-customer-three", name:"test-name-three", clientURI:"http://test-client-uri-three.com")
-    		pro3.addToVens(Ven3)
-    		pro3.save();
+            def hostname = java.net.InetAddress.getLocalHost().getHostName()
+            // FIXME assuming scheme and port :(
+            def serverURL = "http://${hostname}:8080/${grailsApplication.metadata['app.name']}"
+            
+            def pro1 = new Program(name:"Program A", marketContext:"$serverURL/program/a")
+            def pro2 = new Program(name:"Program B", marketContext:"$serverURL/program/b")
+            def pro3 = new Program(name:"Program C", marketContext:"$serverURL/program/c")
+
+            def ven1 = new Ven(venID:"ven1", name:"Site 1", clientURI:"$serverURL/OADRTest/ven/ven1")
+            def ven2 = new Ven(venID:"ven2", name:"Site 2", clientURI:"$serverURL/OADRTest/ven/ven2")
+            def ven3 = new Ven(venID:"ven3", name:"Site 3")
+
+            pro1.addToVens ven1
+            pro2.addToVens ven2
+            pro3.addToVens ven3
+            
+            pro1.save()
+            pro2.save()
+            pro3.save()
+            log.info "+++++ Created bootstrap data!"
         }
         catch ( ex ) {
             log.warn "Bootstrap error (maybe these already exist?", ex
         }
-	}
+    }
     
     def destroy = {
     }
